f55aa3f79227d5e06bfa16bfcf8e3a4a88759988
Jonatan Geovanni Grajales Díaz <jhongeo@jhongeos-iMac.local>
2017-06-23 17:54:05 -0500

    Automatically redirect

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-echo "Hola Gio";
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="refresh" content="0; url=https://sindiciumtechnologies.herokuapp.com/template/index.html">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
