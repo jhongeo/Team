40fe84cd9bafa3fb78d9e65c27ce00ecdc3563ca
Jonatan Geovanni Grajales Díaz <jhongeo@jhongeos-iMac.local>
2017-06-26 10:09:07 -0500

    sindicium logo added, videos updated

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="description" content="Sindicium technologies, sindicium, automatización, sindiciumti, retail, automatización de descarga de información, automatización de descarga de portales web.">
     <meta http-equiv="refresh" content="0; url=https://sindiciumtechnologies.herokuapp.com/template/index.html">
     <title></title>
   </head>
   <body>
-
   </body>
 </html>
